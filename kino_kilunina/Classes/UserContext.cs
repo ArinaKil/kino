aee0aa1806f63169c5a2ebae642568d8395ed392
axxuetam <axxuetam@gmail.com>
2026-05-16 09:24:56 +0500

    Разработанные контексты для моделей. kilunina

---
+++ kino_kilunina/Classes/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kino_kilunina.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace kino_kilunina.Classes
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UserContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Common.Config.ConnectionConfig, Common.Config.Version);
+        }
+    }
+}
