2eb74e395b4bc9109311d813e6c6f45b4616c3a5
axxuetam <axxuetam@gmail.com>
2026-05-16 08:49:21 +0500

    Добавьте файлы проекта.

---
+++ kino_kilunina/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace kino_kilunina
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
